b4e93368113903659803281f5b744ac853e01692
nanoandrew4 <andressmithdev@pm.me>
2025-12-21 01:28:48 +0100

    Remove old lints and test runner
    Signed-off-by: nanoandrew4 <andressmithdev@pm.me>

--- dylint_lints/contract_lints/ui/src/api/rest/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod dto;
-pub mod de0201_test;
-pub mod de0203_test;
-pub mod de0204_test;
-pub mod de0801_test;
-
--- dylint_lints/contract_lints/ui/src/contract/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod de0101_test;
-pub mod de0102_test;
-pub mod de0103_test;
-
